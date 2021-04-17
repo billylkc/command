f46c67a9df1ff22bd30771e789690babee9a3ce2
Billy Lam <0chun0@gmail.com>
2021-04-17 13:29:22 +0800

    w/e

--- cmd/tags.go
+++ cmd/tags.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/billylkc/myutil"
@@ -24,15 +26,45 @@ var gitTagCmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
-		fmt.Println("--------------")
-		fmt.Printf("out: %+v\n", out)
-		fmt.Println("--------------")
-		b := strings.ContainsAny(out, "nothing to commit, working tree clean")
+
+		// Check status first
+		b := strings.Contains(out, "nothing to commit, working tree clean")
 		if !b { // Need commit
-			fmt.Errorf("Need to commit first. \n")
+			fmt.Println("Need to commit first. \n")
+			fmt.Println("Exit")
 			os.Exit(0)
 		}
-		fmt.Println("Continue")
+
+		// Get current tags
+		err, out, _ = myutil.Shell("git tag")
+		if err != nil {
+			fmt.Println(err)
+		}
+		res := strings.Split(out, "\n")
+		current := res[len(res)-2] // current version
+		fmt.Printf("Current version: %s\n", current)
+
+		// New tags
+		version, err := incrementVersion(current)
+		if err != nil {
+			return err
+		}
+
+		// Tag and push to origin
+		err, _, _ = myutil.Shell(fmt.Sprintf("git tag %s", version))
+		if err != nil {
+			return err
+		}
+		fmt.Printf("New version: %s\n", version)
+
+		err, out, _ = myutil.Shell(fmt.Sprintf("git push origin %s", version))
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Push to origin.")
+		fmt.Println("")
+		fmt.Println(out)
 
 		return nil
 	},
@@ -40,4 +72,28 @@ var gitTagCmd = &cobra.Command{
 
 func init() {
 	utilityCmd.AddCommand(gitTagCmd)
+}
+
+func incrementVersion(s string) (string, error) {
+	var version string
+
+	re := "(v\\d\\.\\d\\.)(\\d)"
+	regex := regexp.MustCompile(re)
+	res := regex.FindStringSubmatch(s)
+
+	if len(res) == 3 {
+		_ = res[0]      // original tag, v0.0.4
+		major := res[1] // first part, v0.0.
+		minor := res[2] // second part 4
+		minorVersion, err := strconv.Atoi(minor)
+		if err != nil {
+			return "", err
+		}
+
+		minorVersion += 1
+		version = major + strconv.Itoa(minorVersion)
+	} else {
+		return "", fmt.Errorf("Invalid tag format. Should be something like this, v0.0.5.\nGot this, %s.\n", s)
+	}
+	return version, nil
 }
